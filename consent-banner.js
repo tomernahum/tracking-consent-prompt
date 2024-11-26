4dd100382a0863332a3283093b3efbf7b4ddf46a
tomernahum <git@tomer.nahum.org>
2024-11-25 23:04:50 -0500

    got the injection working

--- consent-banner.js
+++ consent-banner.js
@@ -4,20 +4,34 @@ const servicesConfiguration = [
     {
         name: 'Posthog',
         url: 'https://posthog.com',
-    },
-    {
-        name: 'Sentry',
-        url: 'https://sentry.io',
+        scriptTag: `
+            <script>
+                !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+                posthog.init('phc_DRljMkwhh9JiwqRSiUpAgfg0oamh3hsDP4voKNHMIcF',{api_host:'https://us.i.posthog.com', person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+                    })
+            </script>
+        `
     },
     // {
-    //     name: 'Umami',
-    //     url: 'https://umami.is',
+    //     name: 'Sentry',
+    //     url: 'https://sentry.io',
+    //     scriptTag: ``
     // },
+    {
+        name: 'Umami',
+        url: 'https://umami.is',
+        scriptTag: `<script defer src="https://umami.ttools.io/script.js" data-website-id="41bc8821-4a0b-4aa9-9bf2-84868492508b"></script>`
+    },
     {
         name: 'Google Analytics',
         url: 'https://developers.google.com/analytics',
-    }
+        scriptTag: `<>`
+    },
 ]
+
+/**
+ * @typedef {Array<{name: string, url: string, scriptTag: string}>} ServicesConfiguration
+ */
 
 
 
@@ -140,7 +154,7 @@ const htmlTemplate = `
             <div class="breaker"> </div>
             <p>
                 I respect your privacy and try not to include sensitive information in my tracking. 
-                However, many people might disagree with the amount of data that these tools collect, in that case just click 'No'. 
+                
             </p>
             
             <div class="breaker"> </div>
@@ -173,22 +187,115 @@ const htmlTemplate = `
 
 
 window.addEventListener('load', () => {
-    const div = document.createElement('div');
-    div.innerHTML = htmlTemplate;
+
+    const saved = checkForSavedConsentSetting();
+    if (saved.saved) {
+        if (saved.consents) {
+            console.log("detected previous consent to tracking")
+            onConsent();
+            return
+        }
+        else {
+            console.log("detected previous rejection of tracking")
+            return;
+        }
+    }
+
+    const bannerDiv = document.createElement('div');
+    bannerDiv.innerHTML = htmlTemplate;
     
-    const servicesSpan = /** @type {HTMLElement} */ (div.querySelector('#servicesSpan'));
+    const servicesSpan = /** @type {HTMLElement} */ (bannerDiv.querySelector('#servicesSpan'));
     servicesSpan.appendChild(getServicesText(servicesConfiguration));
 
-    const yesButton = /** @type {HTMLElement} */ (div.querySelector('#yesButton'));
-    const noButton = /** @type {HTMLElement} */ (div.querySelector('#noButton'));
+    const yesButton = /** @type {HTMLElement} */ (bannerDiv.querySelector('#yesButton'));
+    const noButton = /** @type {HTMLElement} */ (bannerDiv.querySelector('#noButton'));
 
     yesButton.addEventListener('click', () => {
+        saveConsentSetting(true);
+        document.body.removeChild(bannerDiv);
+        onConsent();
     })
     noButton.addEventListener('click', () => {
+        saveConsentSetting(false);
+        document.body.removeChild(bannerDiv);
     })
 
-    document.body.appendChild(div);
+    document.body.appendChild(bannerDiv);
 })
+
+/**
+ * Injects a script tag into the document from a string.
+ * @param {string} scriptText - The text of the script tag, including the opening and closing tags You are allowed to put in multiple script tags as well.
+ * @param {string} scriptName - The name of the script tag, only used for debugging
+ */
+function injectScriptFromText(scriptText, scriptName = 'script') {
+    // get attributes
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = scriptText;
+
+    const originalScripts = tempDiv.querySelectorAll('script'); // can't use directly for whatever reason
+
+    if (originalScripts.length == 0) {
+        console.error('No <script> tag found in the provided string', scriptName, scriptText);
+    }
+
+    originalScripts.forEach(originalScript => {
+        const scriptToAppend = document.createElement('script');
+
+        for (const attr of originalScript.attributes) {
+            originalScript.setAttribute(attr.name, attr.value);
+        }
+
+        
+        scriptToAppend.innerHTML = originalScript.innerHTML;
+        // scriptToAppend.textContent = originalScript.textContent;
+
+        document.head.appendChild(scriptToAppend);
+    })
+}
+
+function onConsent(){
+    console.log("attempting to load scripts")
+
+
+    servicesConfiguration.forEach(service => {
+        injectScriptFromText(service.scriptTag, service.name);
+    });
+
+
+
+}
+
+function checkForSavedConsentSetting() {
+    const choice = localStorage.getItem('ttoolsTrackingConsent');
+    
+    if (choice == null) {
+        console.log("no consent setting found")
+        return {
+            saved: false
+        }
+    }
+
+    console.log(choice)
+
+    if (choice == 'true') {
+        return {
+            saved: true,
+            consents: true
+        }
+    }
+
+    return {
+        saved: true,
+        consents: false
+    }
+}
+/**
+ * @param {boolean} consents
+ */
+function saveConsentSetting(consents) {
+    localStorage.setItem('ttoolsTrackingConsent', JSON.stringify(consents));
+}
 
 
 /**
